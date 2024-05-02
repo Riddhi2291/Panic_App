f5394afcfe9e11964dea396f67355be596f2b0cc
Riddhi2291 <riddhi2291@gmail.com>
2024-05-01 20:44:49 -0400

    feat: Add Assets - Icons and Colors

---
+++ src/assets/Icons/index.js
@@ -0,0 +1,6 @@
+export const IconBack = require('./icon_back.png');
+export const AppIcon = require('./appIcon.png');
+export const ArrowDown = require('./icon_Arrow_Down.png');
+export const BackBlack = require('./icon_back_black.png');
+export const Search = require('./icon_search.png');
+export const Check = require('./icon_check.png');
---
+++ src/assets/colors.ts
@@ -0,0 +1,20 @@
+const colors = {
+  transparent: 'transparent',
+  white: '#ffffff',
+  black: '#000000',
+  textPrimary: '#2E2E2E',
+  textSecondary: '#4C6078',
+  green: '#37CCA0',
+  lightGray: '#B0B0B0',
+  lightGreen: '#EDFDF8',
+  red: '#FF6E6E',
+  lightRed: '#FFFBFA',
+  darkRed: '#F04438',
+  disable: '#F6F6F6',
+  placeholder: '#696A6C',
+  placeHolderBackground: '#EEEEEE',
+  shadow: 'rgba(0, 0, 0, 0.4)',
+  loaderBackground: 'rgba(0, 0, 0, 0.7)',
+};
+
+export default colors;
