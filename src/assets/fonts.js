44d525cd390f851ac2d463b34d9f87869d1db87d
Riddhi2291 <riddhi2291@gmail.com>
2024-05-01 20:43:48 -0400

    feat: Config custom fonts
    Add configuration for custom font - WorkSans

---
+++ src/assets/fonts.js
@@ -0,0 +1,12 @@
+/** Define all your fonts here */
+export default {
+  default: 'WorkSans-Regular',
+  'worksans-regular': 'WorkSans-Regular',
+  'worksans-black': 'WorkSans-Black',
+  'worksans-bold': 'WorkSans-Bold',
+  'worksans-semibold': 'WorkSans-SemiBold',
+  'worksans-extrabold': 'WorkSans-ExtraBold',
+  'worksans-light': 'WorkSans-Light',
+  'worksans-medium': 'WorkSans-Medium',
+  'worksans-thin': 'WorkSans-Thin',
+};
